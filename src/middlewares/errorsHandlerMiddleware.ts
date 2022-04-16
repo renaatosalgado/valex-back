80011886209b03ed4f33405fd1cad7617fd5c053
renaatosalgado <renaato.salgado@gmail.com>
2022-04-16 11:49:18 -0300

    fix: deploy config

--- src/middlewares/errorsHandlerMiddleware.ts
+++ src/middlewares/errorsHandlerMiddleware.ts
@@ -8,10 +8,14 @@ export function errorsHandlerMiddleware(
 ) {
   if (err.type === "invalid_input")
     return res.status(err.code).send(err.message);
+
   if (err.type === "unauthorized")
     return res.status(err.code).send(err.message);
+
   if (err.type === "not_found") return res.status(err.code).send(err.message);
+
   if (err.type === "conflict") return res.status(err.code).send(err.message);
+
   if (err.type === "forbidden") return res.status(err.code).send(err.message);
 
   res.sendStatus(500);
