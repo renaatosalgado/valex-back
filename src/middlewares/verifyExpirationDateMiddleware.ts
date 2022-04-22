d3bfc5b4d9a9af90fda5b52d8537b65a09e729d8
Eduardo Vedooto <vedootoeduardo@gmail.com>
2022-04-22 11:06:58 -0300

    Preparando revisão de código

--- src/middlewares/verifyExpirationDateMiddleware.ts
+++
@@ -1,8 +0,0 @@
-import dayjs from "dayjs";
-import * as errorTypes from "../utils/errorTypes.js";
-
-export function verifyExpirationDate(expirationDate: string) {
-  const today = dayjs().format("MM/YY");
-  if (dayjs(today).isAfter(dayjs(expirationDate)))
-    throw errorTypes.conflict("This card has already expired.");
-}
